d273ed73ab5ca0f13279c63cb4988dd1c8e77d18
Krzysztof Krawczyk <44506319+kkrawczykpl@users.noreply.github.com>
2019-03-24 11:55:19 +0100

    Finish HTML part, adding modal

--- index.php
+++ index.php
@@ -27,19 +27,48 @@
 	<div class="beam"></div>
 	<div class="quotes">
 		<div class="pure-g text-center">
-	    <div class="pure-u-1-3">
-	    	<p class="quote">A niech to! Jeżeli panom tak bardzo na tym zależy, to spuszczę wam przysłowiowy wpierdol!</p>
-	    	<p class="text-right caption">-Wściekły Wąż</p>
-	    </div>
-	    <div class="pure-u-1-3">
-	    	<p class="quote">Co prawda ja jestem najlepszy, ale on jest jeszcze lepszy.</p>
-	    	<p class="text-right caption">-Wściekły Wąż</p>
-	    </div>
-	    <div class="pure-u-1-3">
-	    	<p class="quote">Mistrz polecił mi unikać niepotrzebnych walek.</p>
-	    	<p class="text-right caption">-Wściekły Wąż</p>
-	    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">A niech to! Jeżeli panom tak bardzo na tym zależy, to spuszczę wam przysłowiowy wpierdol!</p>
+		    	<p class="text-right caption">-Wściekły Wąż</p>
+		    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">Co prawda ja jestem najlepszy, ale on jest jeszcze lepszy.</p>
+		    	<p class="text-right caption">-Wściekły Wąż</p>
+		    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">Mistrz polecił mi unikać niepotrzebnych walek.</p>
+		    	<p class="text-right caption">-Wściekły Wąż</p>
+		    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">– Panie kapitanie, a jak jakiś kosmita ma w miejscu oczu jajca, i tak tutaj, zamiast buzi i nosa, taki kutacz, to czy on czuje i widzi?<br>
+				– Czuje i widzi.<br>
+				– No, ale jak?<br>
+				– Chujowo.</p>
+		    	<p class="text-right caption">-Kapitan Bomba, Tępy Chuj</p>
+		    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">Chatko na kurzych kulasach, rozdepcz tego kutasa!</p>
+		    	<p class="text-right caption">-Baba Jaga</p>
+		    </div>
+		    <div class="pure-u-1-3">
+		    	<p class="quote">Każda dama ma cenę,<br>Trzeba po prostu pytać, a nie czaić się z kwiatami.</p>
+		    	<p class="text-right caption">-Bracia Figo Fagot: Bożenka</p>
+		    </div>
 		</div>
 	</div>
+	<footer>
+		<p>© 2019 WalaszkoweCytaty, <a href="https://kkrawczyk.pl/">Krzysztof Krawczyk</a></p>
+	</footer>
+
+	<script>
+		window.onload = function()
+		{
+		    const _showed = localStorage.getItem('modal_showed');
+		    if (_showed === null) {
+		        localStorage.setItem('modal_showed', 1);
+		        $('#myModal').modal('show');
+		    }
+		}
+	</script>
 </body>
 </html>
