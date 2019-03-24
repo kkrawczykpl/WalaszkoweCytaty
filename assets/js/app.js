84abedfd72e1542fcf1c2e7f9e841fecfac7d727
Krzysztof Krawczyk <44506319+kkrawczykpl@users.noreply.github.com>
2019-03-24 14:31:09 +0100

    Fix broken 'Change Font' Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,29 +2,29 @@ window.onload = function()
 {
 	MicroModal.init();
     const _showed = localStorage.getItem('modal_showed');
-    const fontBtn = document.querySelector('.change-font');
+    const fontBtn = document.querySelectorAll('.change-font');
     // Modal
     if (_showed === null) {
         localStorage.setItem('modal_showed', 1);
-    	MicroModal.show('modal-1');
     }else{
+    	MicroModal.show('modal-1');
     	//debug
     }
 
     // Change font
-    fontBtn.onclick = function() {
-    	let quotes = document.querySelectorAll('.quote');
-    	for (let i of quotes) {
-    		if (i.style.fontFamily == "Roboto") {
-		    	i.style.fontFamily = "Parisienne";
-		    	i.style.fontSize = "32px";
-		    	fontBtn.style.color = "#777";
-    		}else{
-		    	i.style.fontFamily = "Roboto";
-		    	i.style.fontSize = "24px";
-		    	fontBtn.style.color = "#fe628e";
-    		}
-    		
-    	}
-    }
+    fontBtn.forEach(btn => btn.addEventListener('click', function(){
+        let quotes = document.querySelectorAll('.quote');
+        for (let i of quotes) {
+            if (i.style.fontFamily == "Roboto") {
+                i.style.fontFamily = "Parisienne";
+                i.style.fontSize = "32px";
+                btn.style.color = "#777";
+            }else{
+                i.style.fontFamily = "Roboto";
+                i.style.fontSize = "24px";
+                btn.style.color = "#fe628e";
+            }
+            
+        }
+    }));
 }
