68cddb1f04ecd4819a8148c37a6e8cd9912724ff
Krzysztof Krawczyk <44506319+kkrawczykpl@users.noreply.github.com>
2019-03-24 15:40:59 +0100

    Set Up DB Config File

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php 
+ob_start();
+
+date_default_timezone_set("Europe/Warsaw");
+
+try {
+	$con = new PDO("mysql:dbname=walaszkowe_cytaty;host=localhost", "root", "");
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+
+?>
